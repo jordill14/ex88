bd79d80076375cc350b1ad792542d3dcf3d9ac63
jordill14 <jordill14@gmail.com>
2015-10-07 19:35:51 +0200

    ex 88 CotxeMaps

---
+++ Exercici8_8/src/CotxeMaps.java
@@ -0,0 +1,43 @@
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ * 
+ */
+
+/**
+ * @author iaw47647361
+ *
+ */
+public class CotxeMaps {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Auto-generated method stub
+		//
+		SortedMap <String, Cotxe> mapa = new TreeMap<String, Cotxe>();
+		//
+		mapa.put("1234 AAA", new Cotxe("Alfa Romeo", "Giulia", 2900, 6));
+		mapa.put("2321 BBB", new Cotxe("Seat", "Ibiza", 1200, 3));
+		mapa.put("8232 FFF", new Cotxe("Ford", "Focus", 2000, 4));
+		mapa.put("3311 FFF", new Cotxe("Ford", "Mondeo", 2200, 4));
+		mapa.put("9381 LLL", new Cotxe("Seat", "Leon", 1600, 4));
+		mapa.put("B 9011 XC", new Cotxe("Hyundai", "Atos", 1500, 3));
+		mapa.put("9234 BHG", new Cotxe("Hyundai", "Santa Fe", 3000, 6));
+		mapa.put("9216 CKK", new Cotxe("Hyundai", "Accent", 2900, 4));
+		
+		Cotxe añadido = new Cotxe();
+		añadido = mapa.put("9216 CKK", new Cotxe("Alfa Romeo", "Giulia", 2900, 6));
+		if(añadido == null){
+			System.out.println("se puede");
+		} else {
+			System.out.println("no se puede");
+		}
+		for(Map.Entry<String, Cotxe> entry : mapa.entrySet()){
+			System.out.println(entry.getKey() + "=>" + entry.getValue());
+		}
+}
+}
